f99600ebfb0229275006e7978d0c9f0c3ff6a6e7
Travis Miller <travis@twooutrally.com>
2022-09-24 00:46:36 -0500

    update because I'm tired...

--- src/Http/MessageTrait.php
+++ src/Http/MessageTrait.php
@@ -10,24 +10,46 @@ trait MessageTrait
 {
     use CloneWithTrait;
 
+    /**
+     * @var StreamInterface
+     */
     protected StreamInterface $body;
 
+    /**
+     * @var array
+     */
     protected array $headers = [];
 
+    /**
+     * @var array
+     */
     protected array $headerNames = [];
 
+    /**
+     * @var string
+     */
     protected string $protocol = '1.1';
 
+    /**
+     * @return string
+     */
     public function getProtocolVersion(): string
     {
         return $this->protocol;
     }
 
+    /**
+     * @param string $version
+     * @return MessageTrait
+     */
     public function withProtocolVersion($version): self
     {
         return $this->cloneWith(['protocol' => $version]);
     }
 
+    /**
+     * @return array
+     */
     public function getHeaders(): array
     {
         $headers = [];
@@ -39,11 +61,19 @@ trait MessageTrait
         return $headers;
     }
 
+    /**
+     * @param string $name
+     * @return bool
+     */
     public function hasHeader($name): bool
     {
         return array_key_exists($name, $this->headerNames);
     }
 
+    /**
+     * @param string $name
+     * @return array
+     */
     public function getHeader($name): array
     {
         if (!$this->hasHeader($name)) {
@@ -53,6 +83,10 @@ trait MessageTrait
         return $this->headers[$this->headerNames[$name]];
     }
 
+    /**
+     * @param string $name
+     * @return string
+     */
     public function getHeaderLine($name): string
     {
         $values = $this->getHeader($name);
@@ -60,6 +94,11 @@ trait MessageTrait
         return implode(',', $values);
     }
 
+    /**
+     * @param string $name
+     * @param array|string $value
+     * @return Request|MessageTrait
+     */
     public function withHeader($name, $value): self
     {
         if (!is_string($name)) {
@@ -82,6 +121,11 @@ trait MessageTrait
         ]);
     }
 
+    /**
+     * @param string $name
+     * @param array|string $value
+     * @return Request|MessageTrait
+     */
     public function withAddedHeader($name, $value): self
     {
         if (!is_string($name)) {
@@ -111,6 +155,10 @@ trait MessageTrait
         ]);
     }
 
+    /**
+     * @param string $name
+     * @return Request|MessageTrait
+     */
     public function withoutHeader($name): self
     {
         $headerNames = $this->headerNames;
@@ -121,13 +169,99 @@ trait MessageTrait
         return $this->cloneWith(['headers' => $headers, 'headerNames' => $headerNames]);
     }
 
+    /**
+     * @return StreamInterface
+     */
     public function getBody(): StreamInterface
     {
         return $this->body;
     }
 
+    /**
+     * @param StreamInterface $body
+     * @return Request|MessageTrait
+     */
     public function withBody(StreamInterface $body): self
     {
         return $this->cloneWith(['body' => $body]);
+    }
+
+    /**
+     * @param array|string|int $value
+     * @return void
+     */
+    protected function formatHeaderValue(array|string|int &$value): void
+    {
+        $value = (array) $value;
+
+        foreach (array_keys($value) as $key) {
+            $value[$key] = trim((string) $value[$key], " \t");
+        }
+    }
+
+    /**
+     * @param string $header
+     * @param array|string|int $values
+     * @return void
+     */
+    protected function validateHeader(string $header, array|string|int $values): void
+    {
+        if (preg_match("@^[!#$%&'*+.^_`|~0-9A-Za-z-]+$@", $header) !== 1) {
+            throw new InvalidArgumentException("Invalid header name provided: $header");
+        }
+
+        $values = (array) $values;
+
+        foreach ($values as $value) {
+            if (preg_match("@^[ \t\x21-\x7E\x80-\xFF]*$@", $value) !== 1) {
+                throw new InvalidArgumentException("Invalid header value provided: $value");
+            }
+        }
+    }
+
+    /**
+     * @param $body
+     * @return $this
+     */
+    protected function setBody($body): self
+    {
+        if (!$body instanceof StreamInterface) {
+            $body = new Stream($body);
+        }
+
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * @param array $headers
+     * @return Request|MessageTrait
+     */
+    protected function setHeaders(array $headers): self
+    {
+        foreach ($headers as $name => $value) {
+            $this->validateHeader($name, $value);
+            $this->headerNames[$name] = mb_strtolower($name);
+
+            if (is_string($value)) {
+                $value = explode(', ', $value);
+            }
+
+            $this->headers[$this->headerNames[$name]] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $method
+     * @return void
+     */
+    protected function validateMethod(string $method): void
+    {
+        if (!in_array($method, static::VALID_METHODS)) {
+            throw new InvalidArgumentException("Invalid http method: $method");
+        }
     }
 }
--- src/Http/Request.php
+++ src/Http/Request.php
@@ -6,188 +6,108 @@ use InvalidArgumentException;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
+use Solar\Microframework\Trait\CloneWithTrait;
 
 class Request implements RequestInterface
 {
-    protected array $headers = [];
+    use CloneWithTrait;
+    use MessageTrait;
 
-    protected string $protocol;
-
-    public function __construct()
-    {
-        $this->protocol = $_SERVER['SERVER_PROTOCOL'];
-    }
+    const VALID_METHODS = [
+        'delete',
+        'get',
+        'head',
+        'options',
+        'patch',
+        'post',
+        'put'
+    ];
 
     /**
-     * @inheritDoc
+     * @var string
      */
-    public function getProtocolVersion(): string
-    {
-        return $this->protocol;
-    }
+    protected string $method;
 
     /**
-     * @inheritDoc
+     * @var string
      */
-    public function withProtocolVersion($version): self
-    {
-        if ($this->protocol === $version) {
-            return $this;
-        }
-
-        $clone = clone $this;
-        $clone->protocol = $version;
-
-        return $clone;
-    }
+    protected string $requestTarget;
 
     /**
-     * @inheritDoc
+     * @var UriInterface
      */
-    public function getHeaders(): array
-    {
-        return $this->headers;
+    protected UriInterface $uri;
+
+    public function __construct(
+        string $method,
+        $uri,
+        array $headers = [],
+        StreamInterface $body = null,
+        string $protocol = '1.1'
+    ) {
+        $this->method = mb_strtolower($method);
+        $this->uri = is_string($uri) ? new Uri($uri) : $uri;
+        $this->setHeaders($headers);
+        $this->setBody($body);
+        $this->protocol = $protocol;
     }
 
     /**
-     * @inheritDoc
+     * @return string
      */
-    public function hasHeader($name): bool
+    public function getRequestTarget(): string
     {
-        return array_key_exists($name, $this->headers);
+        return $this->requestTarget;
     }
 
     /**
-     * @inheritDoc
+     * @param string $requestTarget
+     * @return $this
      */
-    public function getHeader($name)
+    public function withRequestTarget($requestTarget): self
     {
-        return $this->headers[$name] ?? [];
+        return $this->cloneWith(['requestTarget' => $requestTarget]);
     }
 
     /**
-     * @inheritDoc
+     * @return string
      */
-    public function getHeaderLine($name)
+    public function getMethod(): string
     {
-        return implode(', ', $this->getHeader($name));
+        return $this->method;
     }
 
     /**
-     * @inheritDoc
+     * @param string $method
+     * @return $this
      */
-    public function withHeader($name, $value): self
+    public function withMethod($method): self
     {
-        $this->validateHeader($name, $value);
-        $this->formatHeaderValue($value);
+        $this->validateMethod($method);
 
-        $clone = clone $this;
-
-        return $clone;
+        return $this->cloneWith(['method' => $method]);
     }
 
     /**
-     * @inheritDoc
+     * @return UriInterface
      */
-    public function withAddedHeader($name, $value)
+    public function getUri(): UriInterface
     {
-        // TODO: Implement withAddedHeader() method.
+        return $this->uri;
     }
 
     /**
-     * @inheritDoc
-     */
-    public function withoutHeader($name)
-    {
-        // TODO: Implement withoutHeader() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getBody()
-    {
-        // TODO: Implement getBody() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function withBody(StreamInterface $body)
-    {
-        // TODO: Implement withBody() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getRequestTarget()
-    {
-        // TODO: Implement getRequestTarget() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function withRequestTarget($requestTarget)
-    {
-        // TODO: Implement withRequestTarget() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getMethod()
-    {
-        // TODO: Implement getMethod() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function withMethod($method)
-    {
-        // TODO: Implement withMethod() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getUri()
-    {
-        // TODO: Implement getUri() method.
-    }
-
-    /**
-     * @inheritDoc
+     * @param UriInterface $uri
+     * @param bool $preserveHost
+     * @return Request
      */
     public function withUri(UriInterface $uri, $preserveHost = false)
     {
-        // TODO: Implement withUri() method.
-    }
-
-    protected function formatHeaderValue(array|string|int &$value)
-    {
-        $value = (array) $value;
-
-        foreach (array_keys($value) as $key) {
-            $value[$key] = trim((string) $value[$key], " \t");
-        }
-    }
-
-    protected function validateHeader(string $header, array|string|int $values)
-    {
-        if (preg_match("@^[!#$%&'*+.^_`|~0-9A-Za-z-]+$@", $header) !== 1) {
-            throw new InvalidArgumentException("Invalid header name provided: $header");
+        if (!$preserveHost) {
+            $uri = $uri->withHost($this->uri->getHost());
         }
 
-        $values = (array) $values;
-
-        foreach ($values as $value) {
-            if (preg_match("@^[ \t\x21-\x7E\x80-\xFF]*$@", $value) !== 1) {
-                throw new InvalidArgumentException("Invalid header value provided: $value");
-            }
-        }
+        return $this->cloneWith(['uri' => $uri]);
     }
 
 
